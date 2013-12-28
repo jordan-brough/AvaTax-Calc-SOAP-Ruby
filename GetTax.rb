a7c98621fb808443a7f4d84a07d20f268f4640c3
Anya Stettler <anya.stettler@avalara.com>
2013-12-27 18:57:03 -0800

    Corrected GetTax request format

--- GetTax.rb
+++ GetTax.rb
@@ -7,111 +7,112 @@ svc = AvaTax::TaxService.new(
   :password => "",  #TODO: Enter your password or license key here
   :clientname => "AvaTaxCalcSOAP Ruby Sample"
   )
-  document = {:companycode=>"SDK", :doctype=>"SalesInvoice", :doccode=>"MyDocCode", :docdate=>"2013-10-11", :salespersoncode=>"Bill Sales", :customercode=>"CUS001", :customerusagetype=>"", :discount=>".0000", :purchaseorderno=>"PO123456", :exemptionno=>"", :origincode=>"123", :destinationcode=>"456", :addresses=>[{:addresscode=>"123", :line1=>"100 ravine lane", :line2=>"Suite 21", :city=>"Bainbridge Island", :region=>"WA", :postalcode=>"98110", :country=>"US", :taxregionid=>"0", :latitude=>"", :longitude=>""}, {:addresscode=>"456", :line1=>"7070 West Arlington Drive", :city=>"Lakewood", :region=>"CO", :postalcode=>"80123", :country=>"US", :taxregionid=>"0"}], :lines=>[{:no=>"1", :itemcode=>"Canoe", :qty=>"1", :amount=>"300.43", :discounted=>"false", :ref1=>"ref1", :ref2=>"ref2", :description=>"Blue canoe", :taxoverridetypeline=>"TaxAmount", :taxamountline=>"10", :taxdateline=>"1900-01-01", :reasonline=>"Tax credit", :taxincluded=>"false"}, {:no=>"2", :itemcode=>"Rowing boat", :qty=>"1", :amount=>"800.12", :discounted=>"false", :ref1=>"ref3", :ref2=>"ref4", :description=>"Red rowing boat", :taxoverridetypeline=>"None", :taxamountline=>"0", :taxdateline=>"1900-01-01", :taxincluded=>"false"}], :detaillevel=>"Tax", :referencecode=>"", :hashcode=>"0", :locationcode=>"", :commit=>"false", :batchcode=>"", :taxoverridetype=>"None", :taxamount=>".0000", :taxdate=>"1900-01-01", :reason=>"", :currencycode=>"USD", :servicemode=>"Remote", :paymentdate=>"2013-09-26", :exchangerate=>".0000", :exchangerateeffdate=>"1900-01-01", :poslanecode=>"", :businessidentificationno=>"", :debug=>true, :validate=>false} 
   #Create the request
   #Document Level Setup  
   #             R: indicates Required Element
   #             O: Indicates Optional Element
   #
-  calc_tax_request = {
-      # Set the tax document properties - Required unless noted as Optional
-      :companycode => "SDK",                          # R: Company Code from the accounts Admin Console
-      :doccode =>"SampleDoc: " + DateTime.now.to_s,   # R: Invoice or document tracking number - Must be unique
-      :doctype => "SalesInvoice",                     # R: Typically SalesOrder,SalesInvoice, ReturnInvoice
-      :docdate => DateTime.now.strftime("%Y-%m-%d"),  # R: Sets reporting date and default tax date
-      :customercode => "TaxSvcTest",                  # R: String - Customer Tracking number or Exemption Customer Code
-      :detaillevel => "Tax",                          # R: Chose Summary, Document, Line or Tax - varying levels of results detail 
-      :commit => "false",                               # O: Default is "false" - Set to "true" to commit the Document
-      #:customerusagetype => "G",                     # O: Send for tax exempt transactions only.
-      #:exemptionno => "12334",                       # O: Send for tax exempt transactions only.
-      #:discount => 0,                                # O: Send for document-level discounts only.
-      :purchaseorderno => "PO 23423",                 # O: Specifies the purchase order number associated with the transaction. This value can be used to track single-use exemption certficates.
-      :referencecode => "",                           # O: This is a reportable value that does not affect tax calculation.
-      :poslanecode => "",                             # O: This is a reportable value that does not affect tax calculation.
-      :salespersoncode => "Bill Sales",               # O: This is a reportable value that does not affect tax calculation.
-      # TaxOverride                                     O: Allows the TaxDate (or other values) to be overridden for tax calculation. Situational only.    
-      #   :taxoverridetype => "TaxDate",
-      #   :reason => "Credit Memo",
-      #   :taxdate => "2011-07-13",
-      #   :taxamount => 0
-      #}]                                        
-      #:businessidentificationno => "",               # O: Specified VAT ID of customer for international/VAT calculations and reporting.
-              
-              
-  #                  Begin Address Section
-  #                  Add the origin and destination addresses referred to by the
-  #                  "setOriginCode" and "setDestinationCode" properties above.
-
-      :addresses => [{
-        :addresscode => "Origin",
-        :line1 => "Avalara",
-        :line2 => "100 Ravine Lane NE",
-        :line3 => "Suite 220",
-        :city => "Bainbridge Island",
-        :region => "WA",
-        :postalcode => "98110",
-        :country => "US"
-        },
-        {
-        :addresscode => "Dest",
-        :line1 => "7462 Kearny Street",
-        :city => "Commerce City",
-        :region => "CO",
-        :postalcode => "80022",
-        :country => "US"    
-        }],
-
-    #
-    # Alternate:  Latitude / Longitude addressing
-    #                                   
-    #   :addresses => [{
-    #   :addressCode => "Origin",
-    #   :latitude => "47.6253",
-    #   :longitude => "-122.515114",
-    #    },{
-    #    :addressCode => "Destination",
-    #    :latitude => "39.833597",
-    #    :longitude => "-104.917220"
-    #    }],                  
-
-  # End Address Section
-
-      # Add invoice lines
+  get_tax_request = {
+    # Set the tax document properties - values are Required unless noted as Optional, but all properties must be defined.
+    
+    :companycode=>"SDK", # R: Company Code from the accounts Admin Console
+    :doctype=>"SalesInvoice", # R: Typically SalesOrder,SalesInvoice, ReturnInvoice
+    :doccode=>"SampleDoc: " + DateTime.now.to_s, # R: Invoice or document tracking number - Must be unique
+    :docdate=>DateTime.now.strftime("%Y-%m-%d"), # R: Sets reporting date and default tax date
+    :salespersoncode=>"Bill Sales", # O: This is a reportable value that does not affect tax calculation.
+    :customercode=>"TaxSvcTestCust", # R: String - Customer Tracking number or Exemption Customer Code
+    :customerusagetype=>"", # O: Send for tax exempt transactions only.
+    :discount=>".0000", # O: Send for document-level discounts only.
+    :purchaseorderno=>"PO123456", # O: Specifies the purchase order number associated with the transaction. This value can be used to track single-use exemption certficates.
+    :exemptionno=>"",  # O: Send for tax exempt transactions only.
+    :origincode=>"123", # R: Value representing the Origin Address
+    :destinationcode=>"456", # R: Value representing the Destination Address
+    :detaillevel=>"Tax", # R: Chose Summary, Document, Line or Tax - varying levels of results detail 
+    :referencecode=>"", # O: This is a reportable value that does not affect tax calculation.
+    :hashcode=>"0", # O: This must be 0
+    :locationcode=>"", # O: This is a reportable value that does not affect tax calculation.
+    :commit=>"false", # O: Default is "false" - Set to "true" to commit the Document
+    :batchcode=>"", # O: This must be left blank.
+      # TaxOverride    O: Allows the TaxDate (or other values) to be overridden for tax calculation. Situational only. 
+      :taxoverridetype=>"None", 
+      :taxamount=>".0000", 
+      :taxdate=>"1900-01-01", 
+      :reason=>"", 
+    :currencycode=>"USD", # O: This is a reportable value that does not affect tax calculation.
+    :servicemode=>"Remote", # O: This is a reportable value that does not affect tax calculation.
+    :paymentdate=>"2013-09-26", # O: This is a reportable value that does not affect tax calculation.
+    :exchangerate=>".0000", # O: This is a reportable value that does not affect tax calculation.
+    :exchangerateeffdate=>"1900-01-01", # O: This is a reportable value that does not affect tax calculation.
+    :poslanecode=>"", # O: This is a reportable value that does not affect tax calculation.
+    :businessidentificationno=>"", # O: Specified VAT ID of customer for international/VAT calculations and reporting.
+    :debug=>true,  # O: If true, transaction logs will be written in the gem root directory.
+    :validate=>false,  # O: If true, tax address information may be returned by the service.
+    
+    #   Address Section
+    #   Add the origin and destination addresses referred to by the
+    #   "OriginCode" and "DestinationCode" properties above.
+    :addresses=>[{
+      :addresscode=>"123", 
+      :line1=>"100 ravine lane", 
+      :line2=>"Suite 21", 
+      :city=>"Bainbridge Island", 
+      :region=>"WA", 
+      :postalcode=>"98110", 
+      :country=>"US", 
+      :taxregionid=>"0", 
+      :latitude=>"", 
+      :longitude=>""
+      }, {
+      :addresscode=>"456", 
+      :line1=>"7070 West Arlington Drive", 
+      :city=>"Lakewood", 
+      :region=>"CO", 
+      :postalcode=>"80123", 
+      :country=>"US", 
+      :taxregionid=>"0"
+      }], 
+    
+    # Add invoice lines
+    :lines=>[{
+      :no=>"1", # R: string - line Number of invoice - must be unique within the document.
+      :itemcode=>"Canoe",  #R: string - SKU or short name of Item
+      :qty=>"1", # R: decimal - The number of items -- Qty of product sold.
+      :amount=>"300.43", # R: decimal - the "NET" amount -- Amount should be the 'extended' or 'net' amount
+      :discounted=>"false", # R: determines if the document-level discount should be applied to this line item.
+      :ref1=>"ref1", # O: This is a reportable value that does not affect tax calculation.
+      :ref2=>"ref2", # O: This is a reportable value that does not affect tax calculation.
+      :description=>"Blue canoe", # O: string - Description or category of item sold.
+            # TaxOverride    O: Allows the TaxDate (or other values) to be overridden for tax calculation. Situational only.
+        :taxoverridetypeline=>"TaxAmount", 
+        :taxamountline=>"10", 
+        :taxdateline=>"1900-01-01", 
+        :reasonline=>"Tax credit", 
+      :taxincluded=>"false" # O: Determines if the line amount on the request includes the tax amount
+      }, {
+      :no=>"2", 
+      :itemcode=>"Rowing boat", 
+      :qty=>"1", 
+      :amount=>"800.12", 
+      :discounted=>"false", 
+      :ref1=>"ref3", 
+      :ref2=>"ref4", 
+      :description=>"Red rowing boat", 
+      :taxoverridetypeline=>"None", 
+      :taxamountline=>"0", 
+      :taxdateline=>"1900-01-01", 
+      :taxincluded=>"false"
+      }]} 
   
-      :lines => [{                             
-      :no => "101",                                   # R: string - line Number of invoice - must be unique.
-      :itemcode => "Item001",                         # R: string - SKU or short name of Item
-      :qty => 1,                                      # R: decimal - The number of items -- Qty of product sold.
-      :amount => 1000.00,                             # R: decimal - the "NET" amount -- Amount should be the 'extended' or 'net' amount
-      #:CustomerUsageType => "G",                     # O: string - AKA Entity Use Code - Typically A - L 
-      :description => "ITEM1",                        # O: string - Description or category of item sold.
-      :taxcode => "",                                 # O: string - Pass standard, custom or Pro-Tax code
-                                                      #             Can be NULL to default to tangible personal property =P0000000)
-      :origincode => "Origin",                        # R: Value representing the Origin Address
-      :destinationcode => "Dest",                     # R: Value representing the Destination Address
-      },{
-      #Line 2 - Shipping/Freight line - See property descriptions above
-      :no => "102",                                   # R: string - SKU or short name of Item
-      :itemcode => "Shipping",                        # R: string - SKU or short name of Item
-      :description => "Shipping- Freight Charges",    # O: string - Description or category of item sold.
-      :qty => 1,                                      # R: decimal - The number of items -- Qty of product sold. Does not function as a mulitplier for Amount
-      :amount => 10.00,                               # R: decimal - the "NET" amount -- Amount should be the 'extended' or 'net' amount
-      :taxcode => "FR",                               # O: string - Pass standard, custom or Pro-Tax code FR020100
-      :origincode => "Origin",                        # R: Value representing the Origin Address
-      :destinationcode => "Dest",                     # R: Value representing the Destination Address
-      }]
-    }
   #Call the service
-result = svc.gettax(document)
-print result
+result = svc.gettax(get_tax_request)
 #Display the result
-print "CalcTax ResultCode: "+result[:ResultCode][0]+"\n"
 
 #If we encountered an error
 if result[:ResultCode][0] != "Success"
-  #Print the first error message returned
-  print result[:Summary][0]+"\n"
+  #Print a message - the gem does not decode the error from the service.
+  print "An error was encountered \n"
 else
+  print "CalcTax ResultCode: "+result[:ResultCode][0]+"\n"
   print "DocCode: " + result[:GetTaxResultDocCode][0]+ " Total Tax Calculated: " + result[:GetTaxResultTotalTax][0].to_s + "\n"
   print "Jurisdiction Breakdown:\n"
   #Show the tax amount calculated at each jurisdictional level
